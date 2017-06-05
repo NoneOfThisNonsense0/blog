33be4a3e88316c8239cba1ce89a6f751bbc54f2c
NoneOfThisNonsense <NoneOfThisNonsense0@gmail.com>
2017-06-04 21:00:31 -0400

    Added editing and creating actions and forms.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,37 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post, notice: "Post successfully updated!"
+    else
+      render :edit
+    end
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: "Post successfully created!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).
+          permit(:title, :content, :category)
+  end
+
 end
